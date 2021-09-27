1474b84b799d82da7ee3d5297592d33c9f74e098
yashgoswami2299 <yashgoswami2299@gmail.com>
2021-09-27 12:49:27 +0530

    SQL-pokemon added

---
+++ Pokemon.sql
@@ -0,0 +1,10 @@
+create database maffooz
+use maffooz
+create table Pokemon(id int primary key auto_increment, name varchar(20) , catagory varchar(20), abilities varchar(20) ,description varchar(300));
+insert into pokemon (name,Catagory,abilities,description) values ('Bulbasaur','Seed','Overgrow','There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger');
+insert into pokemon (name,Catagory,abilities,description) values ('Ivysaur','Seed','Overgrow','When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.');
+insert into pokemon (name,Catagory,abilities,description) values ('Charmeleon','Flame','Blaze','It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.');
+insert into pokemon (name,Catagory,abilities,description) values ('Metapod','Cocoon','Shed Skin','It is waiting for the moment to evolve. At this stage, it can only harden, so it remains motionless to avoid attack.');
+insert into pokemon (name,Catagory,abilities,description) values ('Butterfree','Butterfly','Compound Eyes','In battle, it flaps its wings at great speed to release highly toxic dust into the air.');
+insert into pokemon (name,Catagory,abilities,description) values ('Blastoise','Shellfish','Torrent','It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell.');
+
